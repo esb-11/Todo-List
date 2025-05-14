8fa97647f62d62e6697a18244f54c249f7bca851
Eduardo <198397564+echo-bravo@users.noreply.github.com>
2025-05-14 18:52:48 -0300

    log pubSub activity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles/styles.css";
+import { pubSub } from "./scripts/pubSub.js";
+
+pubSub.emit("init");
--- src/scripts/pubSub.js
+++ src/scripts/pubSub.js
@@ -2,24 +2,29 @@ const events = {};
 
 function on(eventName, fn) {
   events[eventName] = events[eventName] || [];
+  console.log(`${fn.name} added to ${eventName}`);
   events[eventName].push(fn);
 }
 
 function off(eventName, fn) {
-    if (events[eventName]) {
-        for (let i = 0; i < events[eventName].length; i++) {
-            if (events[eventName][i === fn]) {
-                events[eventName].splice(i, 1);
-                break;
-            }
-        }
+  if (events[eventName]) {
+    for (let i = 0; i < events[eventName].length; i++) {
+      if (events[eventName][i === fn]) {
+        events[eventName].splice(i, 1);
+        console.log(`${fn.name} removed from ${eventName}`);
+        break;
+      }
     }
+  }
 }
 
 function emit(eventName, ...data) {
-    if (events[eventName]) {
-        events[eventName].forEach((fn) => { fn(...data) });
-    }
+  if (events[eventName]) {
+    events[eventName].forEach((fn) => {
+      console.log(`${eventName}: ${fn.name}(${data})`);
+      fn(...data);
+    });
+  }
 }
 
 const pubSub = { on, off, emit };
