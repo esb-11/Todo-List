c5590ba0794b0a9fe39cc5ee68c250b93c6e0908
Eduardo <198397564+echo-bravo@users.noreply.github.com>
2025-05-14 18:33:13 -0300

    add pubSub

---
+++ src/scripts/pubSub.js
@@ -0,0 +1,27 @@
+const events = {};
+
+function on(eventName, fn) {
+  events[eventName] = events[eventName] || [];
+  events[eventName].push(fn);
+}
+
+function off(eventName, fn) {
+    if (events[eventName]) {
+        for (let i = 0; i < events[eventName].length; i++) {
+            if (events[eventName][i === fn]) {
+                events[eventName].splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+function emit(eventName, ...data) {
+    if (events[eventName]) {
+        events[eventName].forEach((fn) => { fn(...data) });
+    }
+}
+
+const pubSub = { on, off, emit };
+
+export { pubSub };
